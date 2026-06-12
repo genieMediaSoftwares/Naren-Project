a78e02664bdd626850ff9bb4e85568cfd879bb7b
genieMediaSoftwares <kkdigitalgrowthsoftwares@gmail.com>
2026-06-12 13:55:22 +0530

    connected neon database

--- naren_ultrasound/settings.py
+++ naren_ultrasound/settings.py
@@ -25,7 +25,13 @@ SECRET_KEY = 'django-insecure-radm@l&!3xn^le31)=6sg7st8de!_yqiz8^%@sflr)h_*gv=39
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['narenultrasound.in', "www.narenultrasound.in"] 
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'www.narenultrasound.in',
+    'narenultrasound.in',
+    '.vercel.app',
+]
 
 
 # Application definition
